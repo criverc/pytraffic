74bd89a3fc120bd2da1f86eba8858f761f4f1dd7
Carlos Rivera <carlos@superkaos.org>
2017-04-19 21:00:19 +0200

    set a more realistic tangent cone
    6 degree field view cone and 18 times ball size as view depth.

--- pytraffic/entities.py
+++ pytraffic/entities.py
@@ -306,7 +306,7 @@ class Ball(object):
 
 
     def __get_visibility_cone(self):
-        return self.trajectory.get_tangent_cone(self.position, 0.35, self.radius*5)
+        return self.trajectory.get_tangent_cone(self.position, 0.1, self.radius*18)
 
 
     def render(self, world, screen):
