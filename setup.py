c2c1e49841013037f6a6c785c48e868612478173
Carlos Rivera <carlos@superkaos.org>
2017-08-31 15:51:33 +0200

    wheel and source packages are properly built

--- setup.py
+++ setup.py
@@ -4,4 +4,6 @@ from setuptools import setup
 setup (name="pytraffic",
        version="0.1",
        packages=['pytraffic'],
+       package_data={'pytraffic':  ['examples/*.py', 'examples/*.png']},
+       include_package_data=True,
        install_requires=['pygame', 'docopt', 'numpy'])
